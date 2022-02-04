5d9cd543504454cf13d92d8871b23c6971af66f2
chanbest <b611219@g.hongik.ac.kr>
2022-02-04 20:43:30 +0900

    파일 구분 위한 임시파일 제출

---
+++ src/data structure/B5430/Basicform.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<string, int> psi;
+typedef pair<int, char> pic;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+   
+
+
+    return 0;
+}
